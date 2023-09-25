7fc6b6a05b6874eae526d70b1132824582f1fd2c
Ibrahim-Omran <omranibraheem705@gmail.com>
2023-09-25 22:48:39 +0300

    add routes (one)

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,16 @@
+class Routes {
+  static const String intitlRoute = '/';
+
+  static const String login = '/login';
+  static const String changeLan = '/changeLan';
+  static const String sendCode = '/sendCode';
+  static const String resetPassword = '/resetPassword';
+  static const String menuHome = '/menuHome';
+  static const String addMeal = '/addMeal';
+  static const String profileHome = '/profileHome';
+  static const String updateProfile = '/updateProfile';
+  static const String setting = '/setting';
+  static const String changePassword = '/changePassword';
+}
+
+class AppRoutes {}
